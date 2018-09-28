762e80118e2d4507953af6ded0a8c74459b54e13
Neil Seejoor <nvseejoor@gmail.com>
2018-09-28 18:36:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 // Write your code in this file!
-function scuberGreetingForFeet(someValue) {
-  let statement = ''
-  if (someValue < 2500) {
-    statement = 'No can do.'
-  } else if (someValue < 2000) {
-    statement = 'I will gladly take your thirty bucks.'
-  } else if (someValue <= 400) {
-    statement = 'This one is on me!'
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!'
   }
-  return statement
 }
