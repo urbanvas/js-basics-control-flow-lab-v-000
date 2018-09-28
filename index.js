96ccb35de47cd16675be85acebff646322392458
Neil Seejoor <nvseejoor@gmail.com>
2018-09-28 18:37:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ function scuberGreetingForFeet(feet) {
     return 'This one is on me!'
   } else if (feet >= 2000 && feet < 2500) {
     return 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500) {
+    
   }
 }
