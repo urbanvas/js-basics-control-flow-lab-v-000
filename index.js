e4d34c6f4d598c85809efc2112807d36e165ff9a
Neil Seejoor <nvseejoor@gmail.com>
2018-09-28 18:44:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!'
+  } else if (feet >= 2000 && feet < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500) {
+    return 'No can do.'
+  }
+}
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
+
+function switchOnCharmFromTip(words) {
+  switch (words) {
+    case 'generous':
+      return 'Thank you so much.'
+      break
+    case 'not as generous':
+      return 'Thank you.'
+      break
+    default:
+      return 'Bye.'
+  }
+}
